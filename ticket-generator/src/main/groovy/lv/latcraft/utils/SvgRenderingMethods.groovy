4688e9f89a0bd03dd6cb53011dbdf4b12a01671e
Andrey Adamovich <andrey@aestasit.com>
2016-07-01 22:05:45 +0300

    embed fonts into project

--- ticket-generator/src/main/groovy/lv/latcraft/utils/SvgRenderingMethods.groovy
+++ ticket-generator/src/main/groovy/lv/latcraft/utils/SvgRenderingMethods.groovy
@@ -5,12 +5,21 @@ import com.google.zxing.EncodeHintType
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
+import org.apache.avalon.framework.configuration.Configuration
+import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder
+import org.apache.avalon.framework.container.ContainerUtil
 import org.apache.batik.transcoder.TranscoderInput
 import org.apache.batik.transcoder.TranscoderOutput
 import org.apache.batik.transcoder.image.PNGTranscoder
 import org.apache.fop.svg.PDFTranscoder
 
+
 import static FileMethods.file
+import static java.lang.Boolean.FALSE
+import static org.apache.batik.transcoder.SVGAbstractTranscoder.KEY_PIXEL_UNIT_TO_MILLIMETER
+import static org.apache.batik.transcoder.XMLAbstractTranscoder.KEY_XML_PARSER_VALIDATING
+import static org.apache.fop.svg.AbstractFOPTranscoder.KEY_AUTO_FONTS
+import static org.apache.fop.svg.AbstractFOPTranscoder.KEY_STROKE_TEXT
 
 class SvgRenderingMethods {
 
@@ -39,6 +48,14 @@ class SvgRenderingMethods {
 
   static File renderPDF(File svgFile) {
     PDFTranscoder t = new PDFTranscoder()
+    DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder()
+    Configuration cfg = cfgBuilder.buildFromFile(new File("fonts/pdf-renderer-cfg.xml"))
+    ContainerUtil.configure(t, cfg)
+    int dpi = 300
+    t.addTranscodingHint(KEY_PIXEL_UNIT_TO_MILLIMETER, new Float((float) (25.4 / dpi)))
+    t.addTranscodingHint(KEY_XML_PARSER_VALIDATING, FALSE)
+    t.addTranscodingHint(KEY_STROKE_TEXT, FALSE)
+    t.addTranscodingHint(KEY_AUTO_FONTS, false)
     String svgURI = svgFile.toURI().toString()
     File pdfFile = file('temporary', '.pdf')
     t.transcode(
