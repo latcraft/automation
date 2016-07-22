59e58b8d470373f2ac98dba3208e3772480d6e94
Andrey Adamovich <andrey@aestasit.com>
2016-07-22 12:38:19 +0300

    added more ticket types

--- ticket-generator/src/test/groovy/lv/latcraft/devternity/tickets/TicketGeneratorTest.groovy
+++ ticket-generator/src/test/groovy/lv/latcraft/devternity/tickets/TicketGeneratorTest.groovy
@@ -9,10 +9,45 @@ class TicketGeneratorTest {
 
   @Test
   void testGenerator() {
-    TicketGenerator.generate([
+    println TicketGenerator.generate([
       name     : 'Andrey Adamovich',
       ticketId : 'RVUhC4YTTonZqj1earUh',
+      product  : 'ARTDT2016',
+      company  : 'Aestas/IT',
+      email    : 'andrey@aestasit.com',
+    ], context)
+    println TicketGenerator.generate([
+      name     : 'Andrey Adamovich',
+      ticketId : 'gQQ0bDIeqHm3fFB2Iqeo',
+      product  : 'AWSDT2016',
+      company  : 'Aestas/IT',
+      email    : 'andrey@aestasit.com',
+    ], context)
+    println TicketGenerator.generate([
+      name     : 'Andrey Adamovich',
+      ticketId : 'K2iYj4QU3LxB6OE2HoJp',
       product  : 'DVTRN2016',
+      company  : 'Aestas/IT',
+      email    : 'andrey@aestasit.com',
+    ], context)
+    println TicketGenerator.generate([
+      name     : 'Andrey Adamovich',
+      ticketId : 'vOXBT8tKu9lCXy2Zo05p',
+      product  : 'KVLDT2016',
+      company  : 'Aestas/IT',
+      email    : 'andrey@aestasit.com',
+    ], context)
+    println TicketGenerator.generate([
+      name     : 'Andrey Adamovich',
+      ticketId : 't5skDAdGAFCB9slqysm1',
+      product  : 'ELKDT2016',
+      company  : 'Aestas/IT',
+      email    : 'andrey@aestasit.com',
+    ], context)
+    println TicketGenerator.generate([
+      name     : 'Andrey Adamovich',
+      ticketId : 'X0HLBGF6iFBSKaMEwwkb',
+      product  : 'SPRDT2016',
       company  : 'Aestas/IT',
       email    : 'andrey@aestasit.com',
     ], context)
